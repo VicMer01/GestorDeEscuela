88413089287dfea734b7f7ad3f3324408550b1d7
Victor María <espartalan@gmail.com>
2024-11-16 15:33:09 -0400

    Creacion de clase materia

--- GestorDeEscuela/Modelos/Estudiante.cs
+++ GestorDeEscuela/Modelos/Estudiante.cs
@@ -4,9 +4,39 @@ public class Estudiantes
      public int IdEstudiante {get; set;}
      public string NombreEstudiante {get; set;}
      public string DireccionEstudiante{get; set;}
-     public TipoDeEscuelaEnum TipoDeEscuela {get; set;} 
-      public List<Aula> ListaDeAula { get; set; }
-      public List<Profesores> ListaDeProfesores { get; set; }
-      public List<Estudiantes> ListaDeEstudiantes { get; set; }
-      public List<Materia> ListaDeMaterias { get; set; }    
+     public string EdadEstudiante{get; set;}
+     public SexoEstudianteEnum SexoEstudiante{get; set;}
+     public EstatusEstudiantesEnum EstatusEstudiante{get; set;}
+      public DateTime FechaIngreso { get; set; }
+      public DateTime FechaSalida { get; set; }
+      public List<Materia> ListaDeMateriasEstudiante { get; set; }  
+
+
+      public Estudiantes (int idEstudiante, string nombreEstudiante, string direccionEstudiante,
+       string edadEstudiante, SexoEstudianteEnum sexoEstudiante, EstatusEstudiantesEnum estatusEstudiante,
+        DateTime fechaIngreso, DateTime fechaSalida)
+        {
+            IdEstudiante = ContadorIdEstudiante++;
+            NombreEstudiante = nombreEstudiante;
+            DireccionEstudiante = direccionEstudiante;
+            EdadEstudiante = edadEstudiante;
+            SexoEstudiante = sexoEstudiante;
+            EstatusEstudiante = estatusEstudiante;
+            FechaIngreso = fechaIngreso;
+            FechaSalida = fechaSalida;
+            ListaDeMateriasEstudiante = new List<Materia>();
+        }
+      
+
+    public enum EstatusEstudiantesEnum
+    {
+        Activo,
+        Inactivo
+    }
+    public enum SexoEstudianteEnum
+    {
+        Masculino,
+        Femenino,
+        Otos
+    }
 }
